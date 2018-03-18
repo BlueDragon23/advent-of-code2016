410c37f772759d16e6819abe8f61a06f5badcd3d
Aidan Goldthorpe <aidan.goldthorpe@gmail.com>
2018-03-19 09:45:16 +1000

    Partway through day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,15 +5,22 @@ use std::fs::File;
 fn main() {
 	let f = File::open("day3.txt").unwrap();
 	let reader = BufReader::new(f);
-
-	let count = reader.lines().fold(0u32, |acc, line| {
-        let values: Vec<u32> = line.unwrap().split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect();
-        let largest = values.iter().max().unwrap();
-        if largest >= &(values.iter().sum::<u32>() - largest) {
-            return acc;
-        } else {
-            return acc + 1;
+    let lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
+	let count = lines.chunks(3).fold(0u32, |mut acc, line| {
+        let values: Vec<u32> = line.iter().fold(Vec::new(), |mut v, line| {
+            v.extend(line.split_whitespace().map(|x| x.parse::<u32>().unwrap()));
+            v
+        });
+        
+        for x in 0..2 {
+            let mut triangle = Vec::new();
+            triangle.extend([values[x], values[x+3], values[x+6]].iter());
+            let largest = triangle.iter().max().unwrap();
+            if largest < &(triangle.iter().sum::<u32>() - largest) {
+                acc += 1;
+            }
         }
+        acc
     });
     
     println!("{}", count);
