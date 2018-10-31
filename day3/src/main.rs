ea9f33fd37b897ec3b9b55b64a54459e9daeebb1
Neil Ashford <ashfordneil0@gmail.com>
2018-10-31 12:40:10 +1000

    made day3 more idiomatic

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,27 +1,27 @@
+use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::fs::File;
 
 fn main() {
-	let f = File::open("day3.txt").unwrap();
-	let reader = BufReader::new(f);
+    let f = File::open("day3.txt").unwrap();
+    let reader = BufReader::new(f);
     let lines: Vec<_> = reader.lines().map(|line| line.unwrap()).collect();
-	let count = lines.chunks(3).fold(0u32, |mut acc, line| {
-        let values: Vec<u32> = line.iter().fold(Vec::new(), |mut v, line| {
-            v.extend(line.split_whitespace().map(|x| x.parse::<u32>().unwrap()));
-            v
-        });
-        
-        for x in 0..2 {
-            let mut triangle = Vec::new();
-            triangle.extend([values[x], values[x+3], values[x+6]].iter());
+    let count = lines
+        // find the triangles
+        .chunks(3)
+        .flat_map(|lines| {
+            let values: Vec<u32> = lines
+                .iter()
+                .flat_map(|line| line.split_whitespace().map(|x| x.parse::<u32>().unwrap()))
+                .collect();
+
+            (0..2).map(move |x| vec![values[x], values[x + 3], values[x + 6]])
+        })
+        // find the triangles which are valid
+        .filter(|triangle| {
             let largest = triangle.iter().max().unwrap();
-            if largest < &(triangle.iter().sum::<u32>() - largest) {
-                acc += 1;
-            }
-        }
-        acc
-    });
-    
+            *largest < triangle.into_iter().sum::<u32>() - *largest
+        }).count();
+
     println!("{}", count);
 }
